a7304c2f29c1bb5c61789c82831289cd040c2faa
Sandro Böhme <sandro.boehme@gmx.de>
2019-01-04 19:48:49 +0100

    fixed tick mark in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='bt-ccxt-store',
    version='1.0',
-   description='A fork of Ed Bartosh's CCXT Store Work with some additions',
+   description='A fork of Ed Bartosh\'s CCXT Store Work with some additions',
    author='Dave Vallance',
 #   author_email='foomail@foo.com',
    packages=['ccxtbt'],  
