a3aa1aaeb23b89f1d3914cd9833ab1c3f71fd4d0
Sandro Böhme <sandro.boehme@gmx.de>
2019-01-04 19:43:27 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='bt-ccxt-store',
+   version='1.0',
+   description='A fork of Ed Bartosh's CCXT Store Work with some additions',
+   author='Dave Vallance',
+#   author_email='foomail@foo.com',
+   packages=['ccxtbt'],  
+   install_requires=['backtrader'], 
+)
