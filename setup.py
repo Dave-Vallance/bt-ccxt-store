4a4abeeaab8fb85158dff760a25f5ddd94d83a81
Sandro Böhme <sandro.boehme@gmx.de>
2019-01-05 21:55:30 +0100

    setup.py: fixed name and added url and license

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
 setup(
-   name='bt-ccxt-store',
+   name='bt_ccxt_store',
    version='1.0',
    description='A fork of Ed Bartosh\'s CCXT Store Work with some additions',
+   url='https://github.com/Dave-Vallance/bt-ccxt-store',
    author='Dave Vallance',
 #   author_email='foomail@foo.com',
+   license='MIT',
    packages=['ccxtbt'],  
    install_requires=['backtrader','ccxt'], 
 )
